0898b9391c1ca735732586cb590cddd12924e67a
Adi Yusuf <adiyusuf25@gmail.com>
2022-02-15 19:26:33 +0700

    commit: test

--- app/src/main/java/id/nicholasp/projectgroup/LoginFormActivity.java
+++ app/src/main/java/id/nicholasp/projectgroup/LoginFormActivity.java
@@ -10,5 +10,7 @@ public class LoginFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        //comment
     }
 }
