374b13ce5d16e3116b8b86ff5f0530cd6f55293a
faridprdtm <faridpridiatama@gmail.com>
2022-02-16 16:04:13 +0700

    Detail Portofolio UI

---
+++ app/src/main/java/id/nicholasp/projectgroup/DetailPortofolioActivity.java
@@ -0,0 +1,14 @@
+package id.nicholasp.projectgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPortofolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_portofolio);
+    }
+}
