85dd64e8f62dafe6b983bf4f290080d10f501516
Adhito <45397405+Adhito@users.noreply.github.com>
2022-02-17 20:38:52 +0700

    - Added URL_GET_ALL_COUPON web API

--- app/src/main/java/id/nicholasp/projectgroup/Configuration.java
+++ app/src/main/java/id/nicholasp/projectgroup/Configuration.java
@@ -3,7 +3,7 @@ package id.nicholasp.projectgroup;
 public class Configuration {
 
     // API URL untuk GET, GET_DETAIL, ADD, UPDATE dan DELETE
-    public static final String ip_adress = "192.168.190.102";
+    public static final String ip_adress = "192.168.5.94";
 
     // API Format
     public static final String API_1    = String.format("http://%s/api_task_group/$NAMA_KELOMPOKNYA/$NAMA_FILE_PHP.php", ip_adress);
@@ -14,6 +14,7 @@ public class Configuration {
     public static final String GET_ALL_PRODUCT      = String.format("http://%s/api_task_group/produk/get_all_produk.php", ip_adress);
     public static final String GET_DETAIL_PRODUCT   = String.format("http://%s/api_task_group/produk/get_detail_produk.php?id_produk=", ip_adress);
     public static final String URL_ADD_PRODUCT      = String.format("http://%s/api_task_group/produk/add_produk.php", ip_adress);
+    public static final String URL_GET_ALL_COUPON   = String.format("http://%s/api_task_group/produk/get_all_coupon.php?id_detail_user=", ip_adress);
 
     // API TAG
     public static final String TAG_JSON_ARRAY = "result";
