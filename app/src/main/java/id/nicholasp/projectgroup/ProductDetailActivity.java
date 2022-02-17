bb154afe86ee66cf1f20d95a5ba1f633acad1070
Adhito <45397405+Adhito@users.noreply.github.com>
2022-02-17 16:50:44 +0700

    - Added logic to prevent spending above current_balance

--- app/src/main/java/id/nicholasp/projectgroup/ProductDetailActivity.java
+++ app/src/main/java/id/nicholasp/projectgroup/ProductDetailActivity.java
@@ -32,13 +32,15 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
     // session
     public static final String SHARED_PREFS = "shared_prefs";
     public static final String ID_KEY = "id_key";
+    public static final String BALANCE_KEY = "balance_key";
+    private int myValue = 0; // nilai awal
 
     TextView txt_pd_seri_produk, txt_pd_nama_produk, txt_pd_nominaltransaksi, txt_kelipatan, txt_pd_nilai_unit, txt_pd_yield, txt_pd_jatuh_tempo, txt_pd_minimum_transaksi, txt_pd_maksimum_transaksi, txt_pd_kelipatan_transaksi, txt_pd_penerbit, txt_pd_jenis_kupon, txt_pd_mata_uang, txt_pd_pembayaran_kupon;
     Button btn_beli, btn_plus, btn_minus;
-    String id_produk, id;
+    String id_produk, id, balance;
     Toolbar toolbar;
     SharedPreferences sharedpreferences;
-    private int myValue = 0; // nilai awal
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +53,10 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-//        txt_pd_seri_produk = findViewById(R.id.txt_pd_seri_produk);
+        // txt_pd_seri_produk = findViewById(R.id.txt_pd_seri_produk);
         txt_pd_nama_produk = findViewById(R.id.txt_pd_nama_produk);
-//        txt_pd_nilai_unit = findViewById(R.id.txt_pd_nilai_unit);
-//        txt_pd_yield = findViewById(R.id.txt_pd_yield);
+        // txt_pd_nilai_unit = findViewById(R.id.txt_pd_nilai_unit);
+        // txt_pd_yield = findViewById(R.id.txt_pd_yield);
         txt_pd_jatuh_tempo = findViewById(R.id.txt_pd_jatuh_tempo);
         txt_pd_minimum_transaksi = findViewById(R.id.txt_pd_minimum_transaksi);
         txt_pd_maksimum_transaksi = findViewById(R.id.txt_pd_maksimum_transaksi);
@@ -62,11 +64,10 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
         txt_pd_penerbit = findViewById(R.id.txt_pd_penerbit);
         txt_kelipatan = findViewById(R.id.txt_pd_kelipatan);
         txt_pd_nominaltransaksi = findViewById(R.id.txt_pd_nominaltransaksi);
-//        txt_pd_jenis_kupon = findViewById(R.id.txt_pd_jenis_kupon);
-//        txt_pd_mata_uang = findViewById(R.id.txt_pd_mata_uang);
-//        txt_pd_pembayaran_kupon = findViewById(R.id.txt_pd_pembayaran_kupon);
+        // txt_pd_jenis_kupon = findViewById(R.id.txt_pd_jenis_kupon);
+        // txt_pd_mata_uang = findViewById(R.id.txt_pd_mata_uang);
+        // txt_pd_pembayaran_kupon = findViewById(R.id.txt_pd_pembayaran_kupon);
         btn_beli = findViewById(R.id.btn_beli);
-
         btn_plus = findViewById(R.id.btn_plus);
         btn_minus = findViewById(R.id.btn_minus);
 
@@ -74,7 +75,6 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
         id_produk = receiveIntent.getStringExtra(Configuration.PGW_ID);
 
         getJSON();
-//        btn_beli.setOnClickListener(this);
 
         btn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -159,32 +159,31 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
             JSONArray result = jsonObject.getJSONArray(Configuration.TAG_JSON_ARRAY);
             JSONObject object = result.getJSONObject(0);
 
-
-//            String seri_produk = object.getString("seri_produk");
+            // String seri_produk = object.getString("seri_produk");
             String nama_produk = object.getString("nama_produk");
-//            String nilai_unit = object.getString("nilai_unit");
-//            String yield = object.getString("yield");
+            // String nilai_unit = object.getString("nilai_unit");
+            // String yield = object.getString("yield");
             String jatuh_tempo = object.getString("jatuh_tempo");
             String minimum_transaksi = object.getString("minimum_transaksi");
             String maksimum_transaksi = object.getString("maksimum_transaksi");
             String kelipatan_transaksi = object.getString("kelipatan_transaksi");
             String penerbit = object.getString("penerbit");
-//            String jenis_kupon = object.getString("jenis_kupon");
-//            String mata_uang = object.getString("mata_uang");
-//            String pembayaran_kupon = object.getString("pembayaran_kupon");
+            // String jenis_kupon = object.getString("jenis_kupon");
+            // String mata_uang = object.getString("mata_uang");
+            // String pembayaran_kupon = object.getString("pembayaran_kupon");
 
-//            txt_pd_seri_produk.setText(seri_produk);
+            // txt_pd_seri_produk.setText(seri_produk);
             txt_pd_nama_produk.setText(nama_produk);
-//            txt_pd_nilai_unit.setText(nilai_unit);
-//            txt_pd_yield.setText(yield);
+            // txt_pd_nilai_unit.setText(nilai_unit);
+            // txt_pd_yield.setText(yield);
             txt_pd_jatuh_tempo.setText(jatuh_tempo);
             txt_pd_minimum_transaksi.setText("Rp " + minimum_transaksi);
             txt_pd_maksimum_transaksi.setText("Rp " + maksimum_transaksi);
             txt_pd_kelipatan_transaksi.setText(kelipatan_transaksi);
             txt_pd_penerbit.setText(penerbit);
-//            txt_pd_jenis_kupon.setText(jenis_kupon);
-//            txt_pd_mata_uang.setText(mata_uang);
-//            txt_pd_pembayaran_kupon.setText(pembayaran_kupon);
+            // txt_pd_jenis_kupon.setText(jenis_kupon);
+            // txt_pd_mata_uang.setText(mata_uang);
+            // txt_pd_pembayaran_kupon.setText(pembayaran_kupon);
 
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -197,34 +196,47 @@ public class ProductDetailActivity extends AppCompatActivity implements View.OnC
         return super.onOptionsItemSelected(item);
     }
 
-//    @Override
-//    public void onClick(View view) {
-//        if (view == btn_beli) {
-//            confirmBuyProduct();
-//        } else {
-//            Toast.makeText(getApplicationContext(), "Potential Other Button", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-
     private void confirmBuyProduct() {
-        final String nominal_transaksi = txt_pd_nominaltransaksi.getText().toString().trim();
+        sharedpreferences = getApplicationContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        balance = sharedpreferences.getString(BALANCE_KEY, null);
+
+        final double total_transaction = Integer.parseInt(txt_pd_nominaltransaksi.getText().toString().trim());
+        final double current_balance = Integer.parseInt(balance);
+
 
         // Show confirmation alert dialogue
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Konfirmasi pembelian obligasi");
-        builder.setMessage("Total nominal : " + nominal_transaksi);
+        builder.setMessage("Total nominal : " + total_transaction);
         builder.setIcon(getResources().getDrawable(android.R.drawable.ic_input_add));
         builder.setCancelable(false);
         builder.setNegativeButton("Cancel", null);
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                buyProduct();
+                validateCurrentBalance();
             }
         });
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    private void validateCurrentBalance() {
+        sharedpreferences = getApplicationContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        balance = sharedpreferences.getString(BALANCE_KEY, null);
+
+        final double total_transaction = Integer.parseInt(txt_pd_nominaltransaksi.getText().toString().trim());
+        final double current_balance = Integer.parseInt(balance);
+
+        // With Validation
+        if(total_transaction > current_balance){
+            Toast.makeText(ProductDetailActivity.this, "Mohon maaf, balance anda tidak mencukupi", Toast.LENGTH_LONG).show();
+            Log.d("Balance Gagal", String.valueOf(current_balance));
+        }
+        else {
+            buyProduct();
+        }
     }
 
     private void buyProduct() {
